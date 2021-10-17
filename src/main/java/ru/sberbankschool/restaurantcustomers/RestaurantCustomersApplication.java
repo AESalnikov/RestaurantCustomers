1d9ed7c67e48cade4cdda099bcc556927ca1c90e
antonsalnikov <apple45rus@gmail.com>
2021-10-17 16:38:57 +0500

    Initial commit

---
+++ src/main/java/ru/sberbankschool/restaurantcustomers/RestaurantCustomersApplication.java
@@ -0,0 +1,13 @@
+package ru.sberbankschool.restaurantcustomers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantCustomersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantCustomersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/sberbankschool/restaurantcustomers/RestaurantCustomersApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sberbankschool.restaurantcustomers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantCustomersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
